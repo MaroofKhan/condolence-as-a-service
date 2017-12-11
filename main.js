e2870cc1b47a2c7817754047bec2031af8a4e589
Maroof Khan <maroof.khan@caree.com>
2017-12-11 15:24:28 +0500

    initial.

---
+++ main.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const Express = require("express")
+const BodyParser = require("body-parser")
+const CORS = require("cors")
+
+const PORT = process.env.PORT || 3001
+
+const application = Express()
+application.use(BodyParser.json())
+application.use(BodyParser.urlencoded({ extended: false }))
+application.use(CORS())
+
+application.get("/condole/:about/:whom/:from", function(req, res) {
+	const about = req.params.about
+	const whom = req.params.whom
+	const from = req.params.from
+	const responses = [
+		"I extend my deepest sympathies to you " + whom + ". May your " + about + " be at peace with our Heavenly Father.",
+		"Please accept my condolences, just know that I am here for you " + whom + ", please don’t hesitate to reach out, especially during this difficult time.",
+		"I am truly sorry to hear of your loss " + whom + ". Please accept our condolences and may our prayers help comfort you.",
+		"Whatever happens, happens for good " + whom + "! I'm sure God had better plans for your " + about + ".",
+		"Every cloud has a silver lining, therefore, every " + about + " trouble will eventually be reolved. Stay strong + " + whom + "!"
+	]
+	const random = Math.floor(Math.random() * responses.length)
+	const condolence = responses[random]
+	res.send(condolence)
+})
+
+const server = require("http").createServer(application)
+server.listen(PORT, function() {
+	console.log("Up and running @ localhost: " + PORT);
+})
+
